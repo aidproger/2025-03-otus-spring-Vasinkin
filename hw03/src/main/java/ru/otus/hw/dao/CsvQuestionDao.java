675a1417ebbf0be192a5a4cfb530b7455982dffc
aidproger <ivan.vasinkin@gmail.com>
2025-12-14 16:00:44 +0300

    Реализованное домашнее задание № 3

---
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,45 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        return readCSVWithQuestions().stream()
+                .map(QuestionDto::toDomainObject)
+                .toList();
+    }
+
+    private List<QuestionDto> readCSVWithQuestions() {
+        try (InputStream questionsResourceInputStream =
+                     CsvQuestionDao.class.getResourceAsStream("/" + fileNameProvider.getTestFileName())) {
+
+            return new CsvToBeanBuilder<QuestionDto>(new InputStreamReader(questionsResourceInputStream))
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .withType(QuestionDto.class)
+                    .build()
+                    .parse();
+
+        } catch (Exception e) {
+            throw new QuestionReadException("Error reading csv resource file '"
+                    + fileNameProvider.getTestFileName() + "'...", e);
+        }
+    }
+
+}
---
+++ hw03/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,75 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import ru.otus.hw.common.QuestionConverter;
+import ru.otus.hw.common.TestDataGenerator;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+@DisplayName("Сервис тестирования пользователя ")
+public class TestServiceImplTest {
+
+    private LocalizedIOService ioService;
+
+    private QuestionDao questionDao;
+
+    private QuestionConverter questionConverter;
+
+    private TestServiceImpl testService;
+
+    @BeforeEach
+    void setUp() {
+        ioService = mock(LocalizedIOService.class);
+        questionDao = mock(QuestionDao.class);
+        questionConverter = mock(QuestionConverter.class);
+        testService = new TestServiceImpl(ioService, questionDao, questionConverter);
+    }
+
+    @DisplayName("должен возвращать правильный результат при тестирование студента по списку вопросов")
+    @Test
+    void shouldCorrectTestingStudentByQuestions() {
+
+        var student = new Student("Ivan", "Ivanov");
+
+        var expectedQuestions = TestDataGenerator.generateExpectedQuestionsForCSVTest();
+        var expectedNumbersOfQuestions = List.of(1, 2, 3, 4, 5, 6);
+
+        given(questionDao.findAll()).willReturn(expectedQuestions);
+        given(ioService.readIntForRangeWithPromptLocalized(1, 3,
+                "TestService.choose.number.of.right.answer",
+                "TestService.choose.number.of.right.answer.error")).willReturn(1);
+        given(ioService.readIntForRangeWithPromptLocalized(1, 4,
+                "TestService.choose.number.of.right.answer",
+                "TestService.choose.number.of.right.answer.error")).willReturn(1);
+        var stringCaptor = ArgumentCaptor.forClass(String.class);
+        var questionCaptor = ArgumentCaptor.forClass(Question.class);
+        var integerCaptor = ArgumentCaptor.forClass(Integer.class);
+
+        var testResult = testService.executeTestFor(student);
+
+        verify(questionDao, times(1)).findAll();
+        verify(ioService, times(1)).printLineLocalized(stringCaptor.capture());
+        verify(ioService, times(8)).printLine(stringCaptor.capture());
+        verify(questionConverter, times(6)).convertQuestionToString(questionCaptor.capture(), integerCaptor.capture());
+
+        var actualCreateQuestionRenderer = questionCaptor.getAllValues();
+        assertThat(actualCreateQuestionRenderer).containsExactlyElementsOf(expectedQuestions);
+        var actualNumbersOfQuestions = integerCaptor.getAllValues();
+        assertThat(actualNumbersOfQuestions).containsExactlyElementsOf(expectedNumbersOfQuestions);
+
+        assertThat(testResult.getAnsweredQuestions()).containsExactlyElementsOf(expectedQuestions);
+        assertThat(testResult.getRightAnswersCount()).isEqualTo(3);
+    }
+}
