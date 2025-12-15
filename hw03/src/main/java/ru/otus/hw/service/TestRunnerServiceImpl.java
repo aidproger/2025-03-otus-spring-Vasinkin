f906baa4b7260382dca9a7489d100f84e835d42f
aidproger <ivan.vasinkin@gmail.com>
2025-12-15 14:16:22 +0300

    Исправлено замечание в ПР

--- hw03/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +14,27 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final ResultService resultService;
 
+    private final LocalizedIOService ioService;
+
     @Override
     public void run() {
-        var student = studentService.determineCurrentStudent();
-        var testResult = testService.executeTestFor(student);
-        resultService.showResult(testResult);
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (QuestionReadException qre) {
+            ioService.printLine("");
+            ioService.printLineLocalized("TestRunnerService.error.reading.questions.from.resource.file");
+            ioService.printLine("");
+        } catch (IllegalArgumentException iae) {
+            ioService.printLine("");
+            ioService.printLineLocalized("TestRunnerService.error.choosing.number.of.answer");
+            ioService.printLine("");
+        } catch (Exception e) {
+            ioService.printLine("");
+            ioService.printLineLocalized("TestRunnerService.error.working.application");
+            ioService.printLine("");
+        }
     }
 
 }
